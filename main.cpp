735370ca2ea0288603adeff1a7a5cfcbb36ffc18
tagirmi <tagirmi@gmail.com>
2018-06-23 19:32:37 +0300

    добавил вызов mylib::version()

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
+#include "lib.h"
+
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
+    std::cout << "Hello World!" << std::endl;    
+    std::cout << "Version " << mylib::version() << std::endl;
+
     return 0;
 }
